13b6bd81c828edbb4448a489836f8d55c2633c21
mkhchyan <94595367+mkhchyan@users.noreply.github.com>
2022-05-31 11:49:22 +0400

    Update saga.js

--- client/src/store/saga.js
+++ client/src/store/saga.js
@@ -9,36 +9,22 @@ const Axios = axios.create({
 
 function* getData(data, navigate) {
     let result = yield Axios.post("http://localhost:5000/getdatabases")
-
     yield put(setData(result.data))
-    // console.log(result.data);
 }
 
 function* getTables({ databaseName, navigate }) {
-    // console.log(databaseName);
     const tables = yield Axios.post(`http://localhost:5000/getTables/${databaseName}`)
-    // console.log(tables.data);
     yield put(setTables(tables.data))
 }
 
 
 function* getTableData({ tableName, databaseName}) {
-    // console.log(tableName);
     const tablesData = yield Axios.post(`http://localhost:5000/getTablesData/${tableName}/${databaseName}` )
     yield put(setTablesData(tablesData.data))
-    // console.log(tablesData.data);
-}
-
-function* deleteTableData({ row, tableName, databaseName, tablesData, navigate }) {
-    console.log('OKK', row.id) ;
-		yield Axios.delete(`http://localhost:5000/deleteTableRow`,  row, row.id, tableName, databaseName )
-		const filteredData = tablesData.filter(data => data.id !== row.id)
-		yield put(setTablesData(filteredData))
 }
 
 export function* rootSaga() {   
     yield takeEvery('GET_DATABASE', getData)
     yield takeEvery('GET_TABLES', getTables)
     yield takeEvery('GET_TABLES_DATA', getTableData)
-    yield takeEvery('DELETE_ROW', deleteTableData)
 }
